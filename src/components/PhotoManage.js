bf3b72658e67fb0c0989694c2a238122a55268c8
Liam Hockley <xdega121@gmail.com>
2020-01-13 21:47:05 +0000

    Add photo deletion mechanism
    This is a partial update to issue #2. It doesn't fully close the issue,
    but addresses the most important function.

--- src/components/PhotoManage.js
+++ src/components/PhotoManage.js
@@ -22,7 +22,27 @@ class PhotoManage extends Component {
           this.setState({ 
             photos: photosList 
           });
+        } else {
+          // if no snap, then there are no photos
+          this.setState({
+            photos: null
+          });
         }
+    });
+  }
+  
+  comonentWillUnmount() {
+    this.props.firebase.photos(this.props.uid).off();
+  }
+
+  deletePhoto = (uid, filename, key) => {
+    this.props.firebase.fileRef(uid, filename).delete().then(() => {
+      this.props.firebase.photos(uid).child(key).remove().then(() => {
+        console.log("Photo Removed!");
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
   }
 
@@ -30,7 +50,7 @@ class PhotoManage extends Component {
     return (
       <div>
         <h2 className="page-subhead mb-3">Manage Photos</h2>
-        { this.state.photos ? this.state.photos.map(item => <PhotoItem {...item} />) : <p className="text-left">You have no photos :( </p> }
+        { this.state.photos ? this.state.photos.map(item => <PhotoItem {...item} uid={this.props.uid} deletePhoto={this.deletePhoto} key={item.pid}/>) : <p className="text-left">You have no photos :( </p> }
       </div>
     );
   }
@@ -39,6 +59,7 @@ class PhotoManage extends Component {
 const PhotoItem = (props) => (
   <div>
     <img className="h-40 mt-3" src={props.url} alt={props.filename} />
+    <button onClick={ () => props.deletePhoto(props.uid, props.filename, props.pid) } className="primary-button-mini block mt-1 mb-5">Delete Photo</button>
   </div>
 );
 
